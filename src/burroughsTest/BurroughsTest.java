78f68cea79070964fb126ed3bee82602086c5cac
dvdshing <dvdshing@gmail.com>
2017-01-31 11:12:06 +0000

    Added year. Added ordinal days. Added method to capitalise the first word of Months and Weeks.

--- src/burroughsTest/BurroughsTest.java
+++ src/burroughsTest/BurroughsTest.java
@@ -5,19 +5,36 @@ import java.util.ArrayList;
 public class BurroughsTest {
 
 	public static void main(String[] args) {
+		// Array to hold the MonthPayDate objects
 		ArrayList<MonthPayDate> monthPayList = new ArrayList<>();
 		
+		// Constructor initialisation
 		for (int i = 0; i <= 11; i++){
 			monthPayList.add(new MonthPayDate(i));
 		}
 		
-		for(MonthPayDate monthPayDate: monthPayList){
-			System.out.println(monthPayDate.getMonthName() + " ");
-			System.out.print("Pay Day is on day " + monthPayDate.getPayDay().getDayOfMonth());
-			System.out.println(" on a " + monthPayDate.getPayDay().getDayOfWeek() + " ");
-			System.out.print("Bonus Pay is on day " + monthPayDate.getBonusDay().getDayOfMonth());
-			System.out.println(" on a " + monthPayDate.getBonusDay().getDayOfWeek() + " ");
+		// Output
+		for(MonthPayDate monthPayDate: monthPayList){	
+			System.out.println(convertToLowerCase(monthPayDate.getMonthName()) + " " + monthPayDate.getCurrentMonthYear());
+			System.out.print("Bonus Pay for the " + convertToLowerCase(monthPayDate.getPreviousMonthName()) + " " + monthPayDate.getPreviousMonthYear() + " is on the " + convertToOrdinal(monthPayDate.getBonusDay().getDayOfMonth()));
+			System.out.println(" on a " + convertToLowerCase(monthPayDate.getBonusDay().getDayOfWeek().toString()) + " ");
+			System.out.print("Pay Day is on the " + convertToOrdinal(monthPayDate.getPayDay().getDayOfMonth()));
+			System.out.println(" on a " + convertToLowerCase(monthPayDate.getPayDay().getDayOfWeek().toString()) + " \n");
 		}
 	}
-
+	
+	// Add a suffix to the end of the day. Only takes into consideration -2 days for this program. 
+	public static String convertToOrdinal(int day){
+		if (day % 10 == 1){
+			return day + "st";
+		}else{
+			return day + "th";
+		}
+	}
+	
+	// Convert the Month and Week from UpperCase to capitalise first letter only.
+	public static String convertToLowerCase(String word){
+		word = word.substring(0, 1) + word.substring(1).toLowerCase();
+		return word;
+	}
 }
--- src/burroughsTest/MonthPayDate.java
+++ src/burroughsTest/MonthPayDate.java
@@ -1,27 +1,50 @@
 package burroughsTest;
 
-import java.time.*;
+import java.time.LocalDate;
 
 public class MonthPayDate {
-	private Month monthName;
+	private String monthName;
+	private String previousMonthName;
+	private int currentMonthYear;
+	private int previousMonthYear;
 	private LocalDate payDay;
 	private LocalDate bonusDay;
 	private LocalDate ld = LocalDate.now();
 	
 	// Constructor
 	public MonthPayDate(int numberOfMonth){
+		// Shortening the ld variable
 		LocalDate fullDate = ld.plusMonths(numberOfMonth);
-		monthName = fullDate.getMonth();
 		
+		this.monthName = fullDate.getMonth().toString();
+		this.previousMonthName = fullDate.minusMonths(1).getMonth().toString();
+		
+		this.currentMonthYear = fullDate.getYear();
+		this.previousMonthYear = fullDate.minusMonths(1).getYear();
+		
+		// Get the last day of the month, which is equivalent to the length of the month
 		LocalDate endMonth = fullDate.withDayOfMonth(fullDate.lengthOfMonth());
 		this.payDay = adjustDayOfWeek(endMonth);
 		
+		// Get the 15th of the month.
 		LocalDate middleMonth = fullDate.withDayOfMonth(15);
 		this.bonusDay = adjustDayOfWeek(middleMonth);
 	}
-	
-	public Month getMonthName(){
+		
+	public String getMonthName(){
 		return this.monthName;
+	}
+	
+	public String getPreviousMonthName(){
+		return this.previousMonthName;
+	}
+	
+	public int getCurrentMonthYear(){
+		return this.currentMonthYear;
+	}
+	
+	public int getPreviousMonthYear(){
+		return this.previousMonthYear;
 	}
 	
 	public LocalDate getPayDay(){
@@ -31,7 +54,8 @@ public class MonthPayDate {
 	public LocalDate getBonusDay(){
 		return this.bonusDay;
 	}
-
+	
+	// Method to adjust the payment dates to the closest Friday if they are on a weekend
 	private LocalDate adjustDayOfWeek(LocalDate localDate){
 		String weekDay = localDate.getDayOfWeek().toString();
 		
